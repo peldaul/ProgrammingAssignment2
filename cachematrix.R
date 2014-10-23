c3af9aafadd461a2a65d3b04e0e5e503c3be7bfd
Stephanie Jakus <stephanie.jakus@gmail.com>
2014-10-23 17:34:49 -0400

    edited completed programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -5,35 +5,35 @@
 ## associated with inv to live. 
 ## It also holds the getter and setter methods for getting and setting a matrix and its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    ## Use superassignment here to set the value of x in the environment where makeCacheMatrix
-    ## is defined or the next parent environment where x is defined.
-    x <<- y  
-    ## Use superassignment here to set the value of inv in the parent environment to set()
-    ## i.e. within the function makeCacheMatrix()
-    inv <<- NULL
-  }
-  get <- function() x
-  ## Use superassignment here to set the value of inv within the function makeCacheMatrix()
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
-  ## display the functions for use
-  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+        inv <- NULL
+        set <- function(y) {
+        ## Use superassignment here to set the value of x in the environment where makeCacheMatrix
+        ## is defined or the next parent environment where x is defined.
+        x <<- y  
+        ## Use superassignment here to set the value of inv in the parent environment to set()
+        ## i.e. within the function makeCacheMatrix()
+        inv <<- NULL
+        }
+        get <- function() x
+        ## Use superassignment here to set the value of inv within the function makeCacheMatrix()
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        ## display the functions for use
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 ## A client function for calling the setter method for inv.
 ## It avoids calculating the inverse if it has already been calculated for the vector x.
 cacheSolve <- function(a, ...) {
-  inv <- a$getinverse()
-  if(!is.null(inv)) {
-    message("getting cached data.")
-    return(inv)
-  }
-  data <- a$get()
-  inv <- solve(data)
-  a$setinverse(inv)
-  inv
+        inv <- a$getinverse()
+        if(!is.null(inv)) {
+          message("getting cached data.")
+          return(inv)
+        }
+        data <- a$get()
+        inv <- solve(data)
+        a$setinverse(inv)
+        inv
 }
 
 ## How to call:
